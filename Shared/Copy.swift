eafddb54a19d9bf944c4b9e8e1ba823c58592aed
vaux <zero.griffin@gmail.com>
2021-09-05 14:38:42 +0200

    Fix typo

--- Shared/Copy.swift
+++ Shared/Copy.swift
@@ -14,7 +14,7 @@ This app is **not** tracking you in anyway, nor sharing any information from you
 
 We make use of Apple's *iCloud* to synchronise your data across your own devices, but no one other than you, not us nor even Apple can access your data.
 
-If you allow **notifications** these are going to be displayed only for giving feedback on actions you take while using the app, we **don't** want to contact you in anyway and especifically we **don't** want to send you **Push Notifications**.
+If you allow **notifications** these are going to be displayed only for giving feedback on actions you take while using the app, we **don't** want to contact you in anyway and specifically we **don't** want to send you **Push Notifications**.
 
 Whatever you do with this app is up to you and we **don't** want to know about it.
 """
