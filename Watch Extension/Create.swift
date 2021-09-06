f74a6e91e2569555dc580109160195e988d5a097
vaux <zero.griffin@gmail.com>
2021-09-06 15:58:09 +0200

    Update watch

---
+++ Watch Extension/Create.swift
@@ -0,0 +1,10 @@
+import SwiftUI
+import Secrets
+
+struct Create: View {
+    let archive: Archive
+    
+    var body: some View {
+        Circle()
+    }
+}
--- Watch Extension/Sidebar.swift
+++ Watch Extension/Sidebar.swift
@@ -5,26 +5,38 @@ struct Sidebar: View {
     let archive: Archive
     
     var body: some View {
-        if archive.secrets.isEmpty {
-            Text("No secrets stored yet, continue on your iPhone or iPad.")
-                .font(.footnote)
-                .foregroundStyle(.secondary)
-                .padding(.horizontal)
-        } else {
-            NavigationView {
-                List(0 ..< archive.secrets.count, id: \.self) { index in
-                    NavigationLink(destination: Reveal(secret: archive.secrets[index])) {
-                        Group {
-                            Text(verbatim: archive.secrets[index].name + "\n")
-                                .font(.footnote)
-                            + Text(archive.secrets[index].date, style: .relative)
-                                .foregroundColor(.secondary)
-                                .font(.caption2)
+        NavigationView {
+            List {
+                if archive.secrets.isEmpty {
+                    Text("Create your first secret")
+                        .foregroundStyle(.secondary)
+                        .padding()
+                        .listRowBackground(Color.clear)
+                } else {
+                    ForEach(0 ..< archive.secrets.count, id: \.self) { index in
+                        NavigationLink(destination: Reveal(secret: archive.secrets[index])) {
+                            Group {
+                                Text(verbatim: archive.secrets[index].name + "\n")
+                                    .font(.footnote)
+                                + Text(archive.secrets[index].date, style: .relative)
+                                    .foregroundColor(.secondary)
+                                    .font(.caption2)
+                            }
+                            .frame(maxWidth: .greatestFiniteMagnitude, alignment: .leading)
                         }
-                        .frame(maxWidth: .greatestFiniteMagnitude, alignment: .leading)
                         .privacySensitive()
                     }
                 }
+                
+                NavigationLink(destination: Create(archive: archive)) {
+                    Image(systemName: "plus.circle.fill")
+                        .font(.largeTitle)
+                        .symbolRenderingMode(.hierarchical)
+                        .foregroundColor(.orange)
+                        .frame(maxWidth: .greatestFiniteMagnitude)
+                }
+                .buttonStyle(.borderedProminent)
+                .listRowBackground(Color.clear)
             }
         }
     }
